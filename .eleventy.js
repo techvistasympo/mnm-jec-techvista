3e0d8cce3363e67e1b0203b786b82ba1c874a9bd
santhoshmk <connectwithsanthoshmk@gmail.com>
2025-03-03 16:39:54 +0530

    making eleventy-1

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+export default function(eleventyConfig) {
+
+    eleventyConfig.addPairedShortcode('comment', (content) => {
+        // Simply ignore the content inside the "comment" tag
+        return '';
+    });
+
+    return {
+        dir: {
+            input: 'templates',     // Templates folder
+            includes: 'includes',    // Includes folder within templates
+            output: 'dist',        // Output folder for the generated site
+            data: 'data'
+        },
+        htmlTemplateEngine: 'njk', // Use Nunjucks for HTML files
+    };
+
+}
